fb9d7f63d61acf840bd75bde984fe51fc28e4808
Willem Meints <willem@mein-design.nl>
2016-12-08 16:40:47 +0100

    Generate empty project structure

---
+++ src/main/java/nl/infosupport/App.java
@@ -0,0 +1,13 @@
+package nl.infosupport;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
